cb5ba9872fafed491ba94c6f24b05f9429a82193
jdreyespaez <jd.reyespaez@gmail.com>
2018-11-22 08:57:19 -0500

    Inicio de clase 26: Promesas

---
+++ clase26-22nov/callback.js
@@ -0,0 +1,21 @@
+var fs = require('fs'),
+    file = './juegos.txt',
+    mewFile = './juegos_CALLBACK.txt'
+
+fs.access(file, fs.F_OK, function(err){
+    if(err){
+        console.log('Archivo inexistente')
+    } else {
+        console.log('El archivo existe')
+        fs.readFile(file, function(err, data){
+            if(err){
+                console.log('No se pudo leer el archivo')
+            } else {
+                console.log('Se ha leído exitosamente el archivo')
+                fs.writeFile(newFile, data, function(err){
+                    return (err) ? console.log('No se pudo copiar el archivo') : console.log('El archivo se ha copiado');
+                })
+            }
+        })
+    }
+})
